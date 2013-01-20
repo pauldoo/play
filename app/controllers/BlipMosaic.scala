961911f59c5ba10f86b828f8ef29d841be09b2dc
Paul Richards <paul.richards@gmail.com>
2013-01-20 16:09:06 +0000

    Messing with google analytics and adsense (gotta make the monies). Also started to poke around with Blipfoto's api.

--- app/controllers/BlipMosaic.scala
+++ app/controllers/BlipMosaic.scala
@@ -1,10 +1,32 @@
 package controllers
 import play.api.mvc.Action
 import play.api.mvc.Controller
+import play.api.libs.ws.WS
+import play.api.libs.ws.Response
+import play.api.libs.concurrent.Promise
+import scala.xml.Node
 
 object BlipMosaic extends Controller {
 
   def index = Action {
     Ok("BOOYA")
+  }
+
+  def generate(user: String) = Action {
+    Async {
+      val baseUrl: String = "http://api.blipfoto.com/get/search/";
+      val promise: Promise[Response] = WS.url(baseUrl).withQueryString(
+        ("api_key", "617a7d96081cde9e7207509a96b516a0"),
+        ("version", "2"),
+        ("format", "XML"),
+        ("query", "by " + user),
+        ("size", "small")).get();
+
+      promise.map { response =>
+        val body : Node = response.xml;
+        val k = (body \\ "thumbnail").text;
+        Ok(k);
+      }
+    }
   }
 }
